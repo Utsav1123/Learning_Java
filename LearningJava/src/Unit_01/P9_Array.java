eaf0ce024bffe0bd241f4cef15643310e3b42bee
Utsav1123 <96931742+Utsav1123@users.noreply.github.com>
2022-04-09 15:47:15 +0530

    commit

--- LearningJava/src/Unit_01/P9_Array.java
+++ LearningJava/src/Unit_01/P9_Array.java
@@ -1,5 +1,61 @@
 package Unit_01;
 
+/* Tasks given below:
+		obj.sortAnArray(array);
+		obj.findTheDuplicateElements(array);
+		obj.findSecondLargestAndSecondSmallestElement(array);
+		obj.leftRotationInAnArray(array); 
+		obj.removeElementInArray(array);//(Optional)
+ */
+
 public class P9_Array {
+
+	public static void main(String[] args) {
+
+		int[] array = { 1, 2, 3, 4, 5, 4, 6, 2, 3 };
+
+		QuestionsOnArray obj = new QuestionsOnArray();
+		obj.sortAnArray(array);
+		obj.findTheDuplicateElements(array);
+		obj.findSecondLargestAndSecondSmallestElement(array);
+		obj.leftRotationInAnArray(array);
+		obj.removeElementInArray(array); // (Optional)
+
+	}
+
+}
+
+class QuestionsOnArray {
+
+	void sortAnArray(int[] arr) {
+
+	}
+
+	void findTheDuplicateElements(int[] arr) {
+	for(int i=0;i<arr.length-1;i++)
+	{
+		for(int j=i+1;j<arr.length;j++)
+		{
+			if(arr[i]==arr[j])
+				
+		}
+	}
+
+	}
+
+	void findSecondLargestAndSecondSmallestElement(int[] arr) {
+		// write code here!
+
+	}
+
+	void leftRotationInAnArray(int[] arr) {
+		// write code here!
+
+	}
+
+	void removeElementInArray(int[] arr) {
+		// write code here!
+
+	}
 
 }
