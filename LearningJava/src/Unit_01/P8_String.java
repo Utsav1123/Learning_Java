98ed8ab4a7db3670f57100360b2a2959cf23075a
Utsav1123 <96931742+Utsav1123@users.noreply.github.com>
2022-04-11 00:53:27 +0530

    commit

--- LearningJava/src/Unit_01/P8_String.java
+++ LearningJava/src/Unit_01/P8_String.java
@@ -1,18 +1,13 @@
 package Unit_01;
 
 /* Problem Statement:
-
 P8_String: 
-
 (Create separate logic in separate function)
-
 F1 - Check the entered string is palindrome or not? 
   		String s = "75457"
   		Output: Yes it is a palindrome or No it is not a palindrome.
-
 F2 - Make a reverse of a string using?
 F3 - String Compare: Check if the strings are equal or not?
-
  * */
 
 public class P8_String {
@@ -21,10 +16,10 @@ public class P8_String {
 
 		QuestionsOnString obj = new QuestionsOnString();
 
-		String s1 = new String("75457");
-		String s2 = new String("7545a");
+		String s1 = new String("Hello");
+		String s2 = new String("MADAM");
 
-		obj.palindromeOrNot(s1);
+		obj.palindromeOrNot(s2);
 		obj.reverseOfAString(s1);
 		obj.stringEqualOrNot(s1, s2);
 
@@ -35,17 +30,29 @@ public class P8_String {
 class QuestionsOnString {
 
 	void palindromeOrNot(String s) {
-		
-
+		// Write Logic Here!
+		String rev = "";
+		for (int i = s.length() - 1; i >= 0; i--)
+			rev += s.charAt(i);
+		if (rev.equals(s))
+			System.out.println("The String is Palindrome");
+		else
+			System.out.println("The String is Not Palindrome");
 	}
 
 	void reverseOfAString(String s) {
 		// Write Logic Here!
-
+		String rev = "";
+		for (int i = s.length() - 1; i >= 0; i--)
+			rev += s.charAt(i);
+		System.out.println("Reversed String: " + rev);
 	}
-	
-	void stringEqualOrNot(String s1,String s2) {
+
+	void stringEqualOrNot(String s1, String s2) {
 		// Write Logic Here!
-		
+		if (s1.equals(s2))
+			System.out.println("s1 and s2 are equal");
+		else
+			System.out.println("s1 and s2 are not equal");
 	}
 }
